39a8a22a3e1670c9382fe9423848b3a9b9b67319
2016054148 <2016054148@hanyang.ac.kr>
2019-09-23 01:16:10 +0900

    update main dart

--- bible_app/lib/main.dart
+++ bible_app/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:bible_app/Screens/main_home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BTMK Portal',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World!'),
-        ),
-      ),
+    return new MaterialApp(
+      title: 'Simple Navigation',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.black,
+          primaryIconTheme: IconThemeData(color: Colors.black),
+          primaryTextTheme: TextTheme(
+              title: TextStyle(color: Colors.black)
+              ),
+          textTheme: TextTheme(title: TextStyle(color: Colors.black))),
+      home: new MainHomeScreen(),
     );
   }
 }
