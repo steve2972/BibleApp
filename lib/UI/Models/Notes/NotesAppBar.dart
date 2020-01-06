0f997e5dd95240f7cd2029f2c6a4e39e9d0c2caa
Steven Song <steve2972@gmail.com>
2020-01-07 01:07:42 +0900

    deleted unneeded code

--- lib/UI/Models/Notes/NotesAppBar.dart
+++ lib/UI/Models/Notes/NotesAppBar.dart
@@ -1,8 +1,6 @@
-import 'package:bible_test2/UI/Screens/NotesPage/NoteEditPage.dart';
 import 'package:bible_test2/UI/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:outline_material_icons/outline_material_icons.dart';
 
 class NotesAppBar extends StatelessWidget {
   const NotesAppBar({
@@ -17,18 +15,6 @@ class NotesAppBar extends StatelessWidget {
       pinned: true,
       //centerTitle: true,
       title: Text("Notes", style: Styles.mainText),
-      actions: <Widget>[
-
-        IconButton(
-          icon: Icon(Icons.add),
-          color: Styles.lightIcon,
-          onPressed: () {Navigator.push(
-        context,
-        CupertinoPageRoute(
-            builder: (context) =>
-                EditNotePage(triggerRefetch: refetchNotesFromDB)));},
-        ),
-      ],
     );
   }
 }
