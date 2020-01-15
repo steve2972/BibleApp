77dae1ffbb7a6868bc99b4e6a5c4a5082a91d49a
Steven Song <steve2972@gmail.com>
2020-01-15 20:58:25 +0900

    new settings pages

--- lib/UI/Screens/MenuPage/SettingsPage/ChurchLifePage.dart
+++ lib/UI/Screens/MenuPage/SettingsPage/ChurchLifePage.dart
@@ -1,12 +1,11 @@
 import 'package:bible_test2/UI/Widgets/CommonAppBar.dart';
 import 'package:flutter/material.dart';
 
-class FriendsPage extends StatelessWidget {
+class ChurchLifePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: commonAppBar(context, "동반자들"),
-      
+      appBar: commonAppBar(context, "지역 생활"),
     );
   }
 }
